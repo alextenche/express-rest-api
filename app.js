a97437ed17b4a7666aa1f85d0d352ba6ac87e530
Alexandru Tenche <alex.tenche@gmail.com>
2016-10-02 17:02:33 +0300

    added cross-domain capability

--- app.js
+++ app.js
@@ -23,7 +23,15 @@ db.once('open', function(){
   console.log('db connection succesfull');
 });
 
-
+app.use(function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,DELETE');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use("/questions", routes);
 
